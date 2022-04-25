f258e9758e39b513e7d45f6490caf32cbd651e76
Michael Kim <59414764+xcellentbird@users.noreply.github.com>
2022-04-25 16:09:20 +0900

    Add files via upload

--- csv_to_table.sql
+++ csv_to_table.sql
@@ -1,6 +1,8 @@
+DROP TABLE full_data;
+
 DROP TABLE data;
 
-CREATE TABLE data( 
+CREATE TABLE full_data( 
     celebrity_id SMALLINT NOT NULL AUTO_INCREMENT, 
     feature TEXT NOT NULL, 
     name VARCHAR(40), 
@@ -10,6 +12,10 @@ CREATE TABLE data(
     );
 
 LOAD DATA LOCAL INFILE '/home/ubuntu/imt_data/imt_data.csv' 
-INTO TABLE data FIELDS TERMINATED BY ',' 
+INTO TABLE full_data FIELDS TERMINATED BY ',' 
 OPTIONALLY ENCLOSED BY '"'
 IGNORE 1 LINES;
+
+CREATE TABLE data AS SELECT * FROM full_data WHERE 1=2;
+
+INSERT INTO data SELECT * FROM full_data WHERE birth NOT IN (0);
